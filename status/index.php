99e41418e52cf000b8d291d9a08a3b82dd4cb864
sdobrovol <sdobrovol@sdobrovol>
2016-09-07 17:48:22 +0300

    `

--- status/index.php
+++ status/index.php
@@ -6,21 +6,27 @@ $paysystem = new PaySystem();
 $statusResult = $paysystem->statusUrl();
 error_log(print_r($statusResult, true));
 
-if ($statusResult['success']) {
+if (isset($statusResult['success'])) {
 
     if (!empty($statusResult['data'])) {
         // Действия по предоставлению услуги пользователю при успешной оплате
+        // Пример:
         // $trObj = new Transaction($statusResult['data']['paymentId']);
         // $trObj->completeTransactionId();
         // ............
-    } elseif (isset($statusResult['answerForPaySystem']['text'])) {
+    }
+
+    if (isset($statusResult['answerForPaySystem']['text'])) {
         exit($statusResult['answerForPaySystem']['text']);
     } elseif (isset($statusResult['answerForPaySystem']['xml'])) {
         header("HTTP/1.0 200");
         header("Content-Type: application/xml");
         exit($statusResult['answerForPaySystem']['xml']);
     }
-    exit('OK');
+    exit(json_encode(array('result' => array('message' => 'OK'))));
 } else {
     // Server not responding
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 }
+
+
